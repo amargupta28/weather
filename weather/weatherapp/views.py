fbdc270131a44848e29466ab997e3f1416ca2202
Amar Gupta <amar17gupta@gmail.com>
2022-07-24 11:02:33 -0400

    UI

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+import requests
+from configparser import ConfigParser
+import os
+
+
+
+#globally setting Config File
+configFileLocation = os.path.dirname(os.path.abspath(__file__))
+conffile = os.path.join(configFileLocation, 'config.txt')
+configur = ConfigParser()
+configur.read(conffile)
+configur.sections()
+key= configur.get('default','api_key')
+url=configur.get('default','url')
 
 
 # Create your views here.
 class CurrentWeather(View):
 
     def get(self,req):
+        print(key)
         return HttpResponse("Thank you!!")
 
 
